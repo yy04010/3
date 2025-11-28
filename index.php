896323a1b408e6ccf493ac6fb803cb4b7c46d9c2
yy04010 <2189035817@qq.com>
2025-11-28 13:54:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $request_uri = $_SERVER['REQUEST_URI'];
 
 // 提取/abc/后面的内容
-if (preg_match('#/abc?(.+)$#', $request_uri, $matches)) {
+if (preg_match('#/abc/(.+)$#', $request_uri, $matches)) {
     $id = $matches[1];
 } else {
     $id = '';
@@ -14,7 +14,7 @@ $target_domain = 'https://B域名.com'; // 请替换为实际域名
 
 // 构建目标URL
 if (!empty($id)) {
-    $target_url = $target_domain . '?' . $id;
+    $target_url = $target_domain . '/' . $id;
 } else {
     $target_url = $target_domain;
 }
